1594243ef41a0f9d91273cb54449c4fcc4d85b53
Nazrul Islam <mhdnazrul511@gmail.com>
2025-11-07 07:25:24 +0600

    Add updated python code

--- Web/generate.py
+++ Web/generate.py
@@ -74,14 +74,10 @@ def generate_readme(Solutions: List[Dict[str, Any]]):
             else:
                 rating_counts[rating] = 1
    
-    content = f"""# Codeforces Solution Archive\n
-Welcome to my automated [Codeforces solution archive](https://mhdnazrul.github.io/Codeforces-Solutions/). This site is automatically updated from my GitHub repo using GitHub Actions and the Codeforces API.
----
-### Find me on:
-
+    content = f"""## Codeforces Solution Archive [link](https://mhdnazrul.github.io/Codeforces-Solutions/) 
+### 📢 Find me on:
 - [Codeforces Solution Archive Website](https://mhdnazrul.github.io/Codeforces-Solutions/) 
 - [GitHub](https://github.com/mhdnazrul) | [Codeforces](https://codeforces.com/profile/nazrulislam_7) | [Facebook](https://www.facebook.com/mhdnazrulislam.me/)
----
 ## 📊 Statistics
 * **Total Problems Solved:** {len(Solutions)}
 * **Solved by Difficulty:**
