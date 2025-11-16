289ddb0d2383cbfdf1cdb3cc5dbe6b4bb1d1bd62
Nazrul Islam <157030697+mhdnazrul@users.noreply.github.com>
2025-11-17 01:54:58 +0600

    Refactor solve function for clarity and efficiency

--- Solutions/C_Cyclic_Merging.cpp
+++ Solutions/C_Cyclic_Merging.cpp
@@ -8,7 +8,6 @@ using ll = long long;
 #define rall(vec) (vec).rbegin(), (vec).rend()
 template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
 template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
-
 void solve() {
     int n;
     cin >> n;
@@ -16,43 +15,40 @@ void solve() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-
     int max_idx = max_element(a.begin(), a.end()) - a.begin();
     rotate(a.begin(), a.begin() + max_idx, a.end());
-
     a.push_back(a[0]);
-
     vector<long long> left_val(n + 1), right_val(n + 1);
     vector<int> st;
-
-    st.push_back(0); 
+    st.push_back(0);
     for (int i = 1; i < n; i++) {
-        while (a[st.back()] <= a[i]) st.pop_back();
-        left_val[i] = a[st.back()];
+        while (!st.empty() && a[st.back()] <= a[i]) st.pop_back();
+        if (st.empty()) {
+            left_val[i] = a[0];
+        } else {
+            left_val[i] = a[st.back()];
+        }
         st.push_back(i);
     }
-
     st.clear();
-    st.push_back(n); 
+    st.push_back(n);
     for (int i = n - 1; i >= 1; i--) {
-        while (a[st.back()] < a[i]) st.pop_back();
+        while (!st.empty() && a[st.back()] < a[i]) st.pop_back();
         right_val[i] = a[st.back()];
         st.push_back(i);
     }
-
     long long ans = 0;
     for (int i = 1; i < n; i++) {
         ans += min(left_val[i], right_val[i]);
     }
     cout << ans << nl;
 }
-
 int32_t main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int test_cases = 1;
     if(!(cin>>test_cases)) return 0;
     for(int tc = 1; tc <= test_cases; tc++){
-    //  cout << "Case #" << tc << ": ";
+    // cout << "Case #" << tc << ": ";
         solve();
     }
     return 0;
