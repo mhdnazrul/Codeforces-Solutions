58e4d2a3aaa8d05164a3d8dbd8251ce1099eee2f
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-01 22:55:51 +0600

    Add new solutions

---
+++ Solutions/B_Multiply_by_2_divide_by_6.cpp
@@ -0,0 +1,32 @@
+// problem link:https://codeforces.com/problemset/problem/1374/B
+// Author: nazrulislam_7
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define nl '\n'
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
+template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    ll n;   cin>>n;
+    ll cnt = 0;
+    while(n>1){
+        if(n%6==0){cnt++; n/=6;}
+        else if(n%3==0){cnt++; n*=2;}
+        else {cout<<-1<<nl; return;}
+    }
+    cout<<cnt<<nl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test_cases = 1;
+    if(!(cin>>test_cases)) return 0;
+    for(int tc = 1; tc <= test_cases; tc++){
+    //  cout << "Case #" << tc << ": ";
+        solve();
+    }
+    return 0;
+}
