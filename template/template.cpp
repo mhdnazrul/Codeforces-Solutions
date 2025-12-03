a821e7dcdeaefe43a5083f9f00bc26c2eb6386ff
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-04 01:47:50 +0600

    Add new solutions

---
+++ template/template.cpp
@@ -0,0 +1,29 @@
+/*
+    author  : nazrulislam_7
+    created : 
+problem Name:  
+problem link: 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define nl '\n'
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
+template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test_cases = 1;
+    //if(!(cin>>test_cases)) return 0;
+    for(int tc = 1; tc <= test_cases; tc++){
+    //  cout << "Case #" << tc << ": ";
+        solve();
+    }
+    return 0;
+}
