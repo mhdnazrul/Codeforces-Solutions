32850a64268ed329e9839fe201e38f7cfe592825
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-09 01:38:03 +0600

    Add new solutions

--- template/template.cpp
+++ template/template.cpp
@@ -1,25 +1,33 @@
-// problem link:
-// Author: nazrulislam_7
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │          🔵🔴🟢       >   Handle:- nazrulislam_7             🟢🔴🔵
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://codeforces.com/problemset/problem/2175/B
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-#define nl '\n'
-#define all(vec) (vec).begin(), (vec).end()
-#define rall(vec) (vec).rbegin(), (vec).rend()
-template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
-template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
+using ll=long long;      using ld=long double;
+using vi=vector<int>;    using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+constexpr ll INF=4e18;   constexpr int MOD=1e9+7;
+const char nl='\n';
 
-void solve() {
+#define sz(x) (int)(x.size())
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+
+template<class T> void in(T &x){cin>>x;}
+template<class T> void in(vector<T> &v){for(auto &x:v) cin>>x;}
+template<class T> void out(const T &x){cout<<x<<'\n';}
+template<class T> void out(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
+
+void run_case(){
     
 }
 
-int32_t main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int test_cases = 1;
-    //if(!(cin>>test_cases)) return 0;
-    for(int tc = 1; tc <= test_cases; tc++){
-    //  cout << "Case #" << tc << ": ";
-        solve();
-    }
+int32_t main(){
+    fastio();      int T=1; 
+    if(!(cin>>T))  return 0;
+    while(T--)     run_case();
     return 0;
 }
