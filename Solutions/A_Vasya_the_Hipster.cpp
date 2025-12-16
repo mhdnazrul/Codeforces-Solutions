eef3e24b550547cd71c6ac8ba345b39a22925341
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-17 01:39:01 +0600

    Add new solutions

---
+++ Solutions/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,33 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: 
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;      using ld=long double;
+using vi=vector<int>;    using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+constexpr ll INF=4e18;   constexpr int MOD=1e9+7;
+const char nl='\n';
+#define sz(x) (int)(x.size())
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+template<class T> void in(T &x){cin>>x;}
+template<class T> void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<class T> void out(const T &x){cout<<x<<'\n';}
+template<class T> void outV(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
+
+void run_case(){
+    int x, y;    cin >> x >> y;
+    cout << min(x, y) << " " << (max(x, y) - min(x, y)) / 2 << endl;
+ 
+}
+
+int32_t main(){
+    fastio();      int T=1; 
+    //if(!(cin>>T))  return 0;
+    while(T--)     run_case();
+    return 0;
+}
