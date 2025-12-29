1acb239de9d5a426c3a07b5bf8e6605d306e7056
Nazrul Islam <mhdnazrul511@gmail.com>
2025-12-30 03:25:12 +0600

    Add new solutions

--- Solutions/C_Production_of_Snowmen.cpp
+++ Solutions/C_Production_of_Snowmen.cpp
@@ -1,14 +1,14 @@
 /*┌────────────────────────────────────────────────────────────────────────────────────┐
   │                        >   Handle:- nazrulislam_7             
   │                        >   Author:- Nazrul Islam                   
-  │Problem Link: https://codeforces.com/contest/2182/problem/A
+  │Problem Link: https://codeforces.com/contest/2182/problem/C
   └────────────────────────────────────────────────────────────────────────────────────┘*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;      using ld=long double;
 using vi=vector<int>;    using vll=vector<ll>;
 using pii=pair<int,int>; using pll=pair<ll,ll>;
-constexpr ll INF=1e9;   constexpr int MOD=1e9+7;
+constexpr ll INF=4e18;   constexpr int MOD=1e9+7;
 const char nl='\n';
 #define sz(x) (int)(x.size())
 #define all(x) begin(x),end(x)
@@ -18,31 +18,36 @@ template<class T> void in(T &x){cin>>x;}
 template<class T> void inV(vector<T> &v){for(auto &x:v) cin>>x;}
 template<class T> void out(const T &x){cout<<x<<'\n';}
 template<class T> void outV(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
+
 void run_case(){
     int n;      in(n);
-    string s;   in(s);
-    int cost1 = 4; 
-    for (int i = 0; i <= n - 4; i++) {
-        int diff = 0;
-        if (s[i] != '2') diff++;
-        if (s[i+1] != '0') diff++;
-        if (s[i+2] != '2') diff++;
-        if (s[i+3] != '6') diff++;        
-        if (diff < cost1) {
-            cost1 = diff;
+    vi a(n), b(n), c(n);   
+    inV(a);inV(b);inV(c);
+
+    ll cnt_AB= 0,cnt_BC = 0;
+    for (int s = 0; s < n; s++) {
+        bool ok = true;
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= b[(i + s) % n]) {
+                ok = false;
+                break;
+            }
         }
+        if (ok) cnt_AB++;
     }
-    int cost2 = 0;
-    for (int i = 0; i <= n - 4; i++) {
-        if (s[i] == '2' && s[i+1] == '0' && s[i+2] == '2' && s[i+3] == '5') {
-            cost2++;
-            i += 3;
+    for (int s = 0; s < n; s++) {
+        bool ok = true;
+        for (int i = 0; i < n; i++) {
+            if (b[i] >= c[(i + s) % n]) {
+                ok = false;
+                break;
+            }
         }
+        if (ok) cnt_BC++;
     }
-    cost1 < cost2?out(cost1):out(cost2);
+    ll ans = (ll)n * cnt_AB * cnt_BC;
+    out(ans);
 }
-
-
 
 int32_t main(){
     fastio();      int T=1; 
