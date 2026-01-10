84d69bec2e7663faa08a029aaebccfa4f42b4269
mhdnazrul <mhdnazrul@users.noreply.github.com>
2026-01-10 19:09:21 +0000

    deploy: da8de66c8288caf2f7cf834d5f1c70477496845a

---
+++ Solutions/A_Buy_a_Shovel.cpp
@@ -0,0 +1,31 @@
+// problem link:https://codeforces.com/problemset/problem/732/A
+// Author: nazrulislam_7
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define nl '\n'
+#define all(vec) (vec).begin(), (vec).end()
+#define rall(vec) (vec).rbegin(), (vec).rend()
+template<class T> void readV(vector<T>& v) { for(auto &x : v) cin >> x; }
+template<class T> void printV(const vector<T>& v) { for(auto x : v) cout << x << ' '; cout << '\n'; }
+
+void solve() {
+    ll k,r;     cin>>k>>r;
+    for(ll i = 1; ;i++){
+        if((k*i)%10 == r || (k*i)%10 == 0){
+            cout<<i<<nl;
+            return;
+        }
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int test_cases = 1;
+    //if(!(cin>>test_cases)) return 0;
+    for(int tc = 1; tc <= test_cases; tc++){
+    //  cout << "Case #" << tc << ": ";
+        solve();
+    }
+    return 0;
+}
---
+++ Solutions/B_New_Year_s_Number.cpp
@@ -0,0 +1,40 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://codeforces.com/problemset/problem/1475/B
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;      using ld=long double;
+using vi=vector<int>;    using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+constexpr ll INF=4e18;   constexpr int MOD=1e9+7;
+const char nl='\n';
+#define sz(x) (int)(x.size())
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+template<class T> void in(T &x){cin>>x;}
+template<class T> void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<class T> void out(const T &x){cout<<x<<'\n';}
+template<class T> void outV(const vector<T> &v){for(int i=0;i<(int)v.size();i++) cout<<v[i]<<(i+1<(int)v.size()?' ':'\n');}
+
+void run_case(){
+    int n;      in(n);
+    bool ok = 0;
+    while(n>=2020){
+        if(n%2020==0 || n%2021==0){
+            ok=1;
+            break;
+        }
+        else n-=2021;
+    }
+    ok?out("YES"):out("NO");
+}
+
+int32_t main(){
+    fastio();      int T=1; 
+    if(!(cin>>T))  return 0;
+    while(T--)     run_case();
+    return 0;
+}
