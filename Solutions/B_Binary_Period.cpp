f4756af0a02473b8394e6cf157ca09719dad1ce6
Nazrul Islam <mhdnazrul511@gmail.com>
2026-01-24 03:33:39 +0600

    Add new solutions

--- Solutions/B_Binary_Period.cpp
+++ Solutions/B_Binary_Period.cpp
@@ -22,7 +22,24 @@ template<typename T> void outV(const vector<T>& v) {if (v.empty()) return;
     for (int i = 0; i < sz(v); ++i)cout << v[i] << " \n"[i == sz(v) - 1];}
 
 void run_case(){
-    
+    string s;    cin>>s;
+    ll a=0,b=0,n=sz(s);
+    rep(i,0,sz(s))
+    {
+        if(s[i]=='0') a++;
+        else b++;
+    }
+    if(a==sz(s) || n==b || n<=2)
+    {
+        cout<<s<<'\n';
+        return;
+    }
+    string a1="10";
+    rep(i,0,n)
+    {
+        cout<<a1;
+    }
+    cout<<'\n';
 }
 
 int32_t main(){
@@ -33,70 +50,3 @@ int32_t main(){
     return 0;
 }
 
-
-
-
-#include<bits/stdc++.h>
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repd(i,a,b) for(ll i=a;i>=b;i--)
-#define all(sdk) sdk.begin(),sdk.end()
-#define ll long long
-#define ull unsigned long long 
-#define mod 1000000007
-#define sz(a) (ll)a.size()
-#define vc vector
-#define st set
-#define pii pair<int,int>
-#define F first
-#define S second
-#define mp make_pair
-#define pb push_back
-#define pop pop_back
-#define gcd(a,b)    __gcd((a),(b))
-#define PI 3.14159265
-#define r0 return 0
-#define printvector(v) for(int i= 0;i<v.size();i++) {cout<<v[i]<<" ";} cout<<endl;
-#define printarr(a,n) for(int i=0;i<n;++i) {cout<<a[i]<<" ";} cout<<endl;
-using namespace std;
-void solve()
-{
-    ll n,k;
-    ll m;
-    string s;
-    cin>>s;
-    n=sz(s);
-    ll a=0,b=0;
-    rep(i,0,sz(s))
-    {
-        if(s[i]=='0')
-            a++;
-        else
-        {
-            b++;
-        }
-    }
-    if(a==sz(s) || n==b || n<=2)
-    {
-        cout<<s<<endl;
-        return;
-    }
-    string a1="10";
-    rep(i,0,n)
-    {
-        cout<<a1;
-    }
-    cout<<endl;
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int TESTS=1;
-    cin>>TESTS;
-    while(TESTS--)
-    {
-        solve();
-    }
-    return 0;
-}
