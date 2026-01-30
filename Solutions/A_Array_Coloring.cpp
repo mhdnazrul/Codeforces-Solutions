96e64ccd2e1df8730ba9e37b94498b04943a63c6
mhdnazrul <mhdnazrul@users.noreply.github.com>
2026-01-30 18:32:50 +0000

    deploy: 780e675960806513cf2c6150d349db7a102699cf

---
+++ Solutions/A_Array_Coloring.cpp
@@ -0,0 +1,48 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://codeforces.com/contest/2191/problem/A
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;       using ld=long double;
+using vi=vector<int>;     using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+const ll INF = 2e18;   const int MOD = 1e9 + 7; 
+#define per(i,a,b)  for(int i=(b)-1;i>=(a);--i)
+#define rep(i,a,b)  for(int i=(a);i<(b);++i)
+#define trav(a, x)     for (auto &a : x)
+#define forr(i, n)       rep(i, 0, n)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) rbegin(x),rend(x)
+#define sz(x) (int)(x).size()
+const char nl = '\n';
+template<class T>    void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<typename T> void outV(const vector<T>& v) {if (v.empty()) return;
+    for (int i = 0; i < sz(v); ++i)cout << v[i] << " \n"[i == sz(v) - 1];}
+
+void run_case(){
+    int n;    cin >> n;    
+    vector<pii> vpp(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vpp[i].first;
+        vpp[i].second = i;
+    }
+    sort(all(vpp));
+
+    bool ok =1;
+    for (int j = 0; j < n - 1; j++) {
+        int crr = vpp[j].second % 2;
+        int pos = vpp[j + 1].second % 2;        
+        if (crr == pos) { ok = 0;break;}
+    }
+    cout <<(ok?"YES\n":"NO\n"); 
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);          int T=1;
+    if(!(cin >> T))     return 0;
+    while(T--)        run_case();
+    return 0;
+}
