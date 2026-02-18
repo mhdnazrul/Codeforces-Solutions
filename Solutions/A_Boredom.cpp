ee4154ae30573e55e89cbf46c68d0c39ebe2e4c6
Nazrul Islam <mhdnazrul511@gmail.com>
2026-02-19 00:50:22 +0600

    Add new solutions

---
+++ Solutions/A_Boredom.cpp
@@ -0,0 +1,45 @@
+/*┌────────────────────────────────────────────────────────────────────────────────────┐
+  │                        >   Handle:- nazrulislam_7             
+  │                        >   Author:- Nazrul Islam                   
+  │Problem Link: https://codeforces.com/contest/455/problem/A
+  └────────────────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;       using ld=long double;
+using vi=vector<int>;     using vll=vector<ll>;
+using pii=pair<int,int>; using pll=pair<ll,ll>;
+const ll INF = 2e18;   const int MOD = 1e9 + 7; 
+#define per(i,a,b)  for(int i=(b)-1;i>=(a);--i)
+#define rep(i,a,b)  for(int i=(a);i<(b);++i)
+#define trav(a, x)     for (auto &a : x)
+#define forr(i, n)       rep(i, 0, n)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) rbegin(x),rend(x)
+#define sz(x) (int)(x).size()
+const char nl = '\n';
+template<class T>    void inV(vector<T> &v){for(auto &x:v) cin>>x;}
+template<typename T> void outV(const vector<T>& v) {if (v.empty()) return;
+    for (int i = 0; i < sz(v); ++i)cout << v[i] << " \n"[i == sz(v) - 1];}
+
+void run_case(){
+    int n;  	cin >> n;
+	vi a(n);    inV(a);
+	ll mx = *max_element(a.begin(), a.end()) + 1;
+
+	vi b(mx);
+	for(int i = 0; i < n; ++i){ ++b[a[i]]; }
+	vll dp(mx + 2);
+	for(int i = 0; i < mx; ++i){
+		dp[i + 1] = max(dp[i + 1], dp[i]);
+		dp[i + 2] = max(dp[i + 2], dp[i] + ll(b[i]) * i);
+	}
+	cout << max(dp[mx], dp[mx + 1]) << nl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);          int T=1;
+    //if(!(cin >> T))     return 0;
+    while(T--)        run_case();
+    return 0;
+}
