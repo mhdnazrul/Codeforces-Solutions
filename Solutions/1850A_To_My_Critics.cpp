502c7ddfc4ac2229d0b91e16f6208b2b83cfe568
Nazrul Islam <157030697+mhdnazrul@users.noreply.github.com>
2026-03-16 03:41:30 +0600

    Sync Codeforces: 1850A - To My Critics [C++23 (GCC 14-64, msys2)]

--- Solutions/1850A_To_My_Critics.cpp
+++ Solutions/1850A_To_My_Critics.cpp
@@ -5,22 +5,6 @@
   └────────────────────────────────────────────────────────────────────────────────────┘*/
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long;       using ld=long double;
-using vi=vector<int>;     using vll=vector<ll>;
-using pii=pair<int,int>; using pll=pair<ll,ll>;
-const ll INF = 2e18;   const int MOD = 1e9 + 7; 
-#define per(i,a,b)  for(int i=(b)-1;i>=(a);--i)
-#define rep(i,a,b)  for(int i=(a);i<(b);++i)
-#define trav(a, x)     for (auto &a : x)
-#define forr(i, n)       rep(i, 0, n)
-#define all(x) (x).begin(),(x).end()
-#define rall(x) rbegin(x),rend(x)
-#define sz(x) (int)(x).size()
-const char nl = '\n';
-template<class T>    void inV(vector<T> &v){for(auto &x:v) cin>>x;}
-template<typename T> void outV(const vector<T>& v) {if (v.empty()) return;
-    for (int i = 0; i < sz(v); ++i)cout << v[i] << " \n"[i == sz(v) - 1];}
-
 void run_case(){
     int a,b,c;      cin>>a>>b>>c;
     if(a+b>=10 || a+c>=10 || b+c>=10)cout<<"YES\n";
