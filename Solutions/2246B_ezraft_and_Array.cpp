f1bd80c4335c58084e7594da9b122692d0a6fa21
Nazrul Islam <157030697+mhdnazrul@users.noreply.github.com>
2026-07-12 20:57:13 +0600

    Sync Codeforces: 2246B - ezraft and Array [C++23 (GCC 14-64, msys2)]

---
+++ Solutions/2246B_ezraft_and_Array.cpp
@@ -0,0 +1,48 @@
+/*┌──────────────────────────────────────────────────────────────────────────┐
+  │           >   Handle: nazrulislam_7  ||  Author: Nazrul Islam
+  |           Problem: B. ezraft and Array
+  │           Problem Link: https://codeforces.com/contest/2246/problem/B
+  └──────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll  = long long;using ld  = long double;using vi  = vector<int>;
+using vll = vector<ll>;using pii = pair<int,int>;using pll = pair<ll,ll>;
+using ull = unsigned long long; // using i128 = __int128; // only use C++ 20 or up
+constexpr ll INF = 1'000'000'000'000'000'000LL;  constexpr int MOD = 1'000'000'007;
+#define rep(i,a,b)  for(int i=(a);i<(b);++i)
+#define per(i,a,b)  for(int i=(b)-1;i>=(a);--i)
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define sz(x)       (int)(x).size()
+#define fi          first
+#define se          second
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n";  }
+template<class T> void Vin(vector<T>& v){ for (auto& x : v) cin >> x; }
+template<class T> void Vout(const vector<T>& v) {
+    rep(i, 0, sz(v)) cout << v[i] << " \n"[i == sz(v) - 1];
+}
+// ──────────────────────────────────Solution──────────────────────────────────
+ 
+void run_case() {
+    int n; cin>>n;
+    if(n==1){cout<<"1\n"; return;}
+    else if(n==2){ cout<<"-1\n";return; }
+ 
+    vll v;
+    for(int i=n-3; i>=0; i--){
+        v.push_back(3LL * (1LL << i));
+    }
+    v.push_back(2ll);
+    v.push_back(1ll);
+    Vout(v);
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) run_case();
+    return 0;
+}
