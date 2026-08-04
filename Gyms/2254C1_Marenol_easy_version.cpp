d37ed43527939699843b51d3aa2161f7a667fad4
Nazrul Islam <157030697+mhdnazrul@users.noreply.github.com>
2026-08-04 21:21:59 +0600

    Sync Codeforces: 2254C1 - Marenol (easy version) [C++23 (GCC 14-64, msys2)]

---
+++ Gyms/2254C1_Marenol_easy_version.cpp
@@ -0,0 +1,57 @@
+/*┌──────────────────────────────────────────────────────────────────────────┐
+  │           >   Handle: nazrulislam_7  ||  Author: Nazrul Islam
+  |           Problem: C1. Marenol (easy version)
+  │           Problem Link: https://codeforces.com/contest/2254/problem/C1
+  └──────────────────────────────────────────────────────────────────────────┘*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll  = long long;using ld  = long double;using vi  = vector<int>;
+using vll = vector<ll>;using pii = pair<int,int>;using pll = pair<ll,ll>;
+using ull = unsigned long long; // using i128 = __int128; // only use C++ 20 or up
+constexpr ll INF = 1'000'000'000'000'000'000LL;  constexpr int MOD = 1'000'000'007;
+#define rep(i,a,b)  for(int i=(a);i<(b);++i)
+#define per(i,a,b)  for(int i=(b)-1;i>=(a);--i)
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define sz(x)       (int)(x).size()
+#define fi          first
+#define se          second
+inline void yes() { cout << "YES\n"; }
+inline void no()  { cout << "NO\n";  }
+template<class T> void Vin(vector<T>& v){ for (auto& x : v) cin >> x; }
+template<class T> void Vout(const vector<T>& v) {
+    rep(i, 0, sz(v)) cout << v[i] << " \n"[i == sz(v) - 1];
+}
+// ──────────────────────────────────Solution──────────────────────────────────
+ 
+void run_case() {
+    int n;    cin >> n;
+    string a, b;    cin >> a >> b;
+    int A_even = 0, A_odd = 0;
+    int B_even = 0, B_odd = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        if (a[i] == '1') {
+            if (i % 2 == 0) A_even++;
+            else A_odd++;
+        }
+        if (b[i] == '1') {
+            if (i % 2 == 0) B_even++;
+            else B_odd++;
+        }
+    }
+ 
+    if (A_even == B_even && A_odd == B_odd) yes();
+    else no();
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) run_case();
+    return 0;
+}
+ 
+ 
